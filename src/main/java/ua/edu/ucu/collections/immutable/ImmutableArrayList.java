c76849ef88aa7309807d13cf95c218944c6f9127
booyakashakawabangha <tatarik.sv@gmail.com>
2021-12-14 13:29:33 +0200

    Started implementing...

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -1,28 +1,56 @@
 package ua.edu.ucu.collections.immutable;
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public final class ImmutableArrayList implements ImmutableList {
+
+    private Object[] array;
+    private int length;
+
+    @Override
+    public String toString() {
+        return "ImmutableArrayList = " + Arrays.toString(array) +
+                ", length=" + length;
+    }
+
     public ImmutableArrayList(Object[] elements) {
+        this.length = elements.length;
+        array = new Object[elements.length];
+        for (int idx = 0; idx < elements.length; idx++){
+            array[idx] = elements[idx];
+        }
     }
 
     public ImmutableArrayList() {
+        array = new Object[0];
     }
 
     @Override
     public ImmutableList add(Object e) {
-        return null;
+        Object[] newArray = Arrays.copyOf(array, length+1);
+        newArray[length] = e;
+        return new ImmutableArrayList(newArray);
     }
 
     @Override
     public ImmutableList add(int index, Object e) {
-        return null;
+        Object[] newArray = Arrays.copyOf(array, length+1);
+        for (int idx = this.length; idx > index; idx--){
+            newArray[idx] = newArray[idx - 1];
+        }
+        newArray[index] = e;
+        return new ImmutableArrayList(newArray);
     }
 
     @Override
     public ImmutableList addAll(Object[] c) {
-        return null;
+        Object[] newArray = Arrays.copyOf(array, length + c.length);
+        for (int idx = 0; idx < c.length; idx++){
+            newArray[idx + length] = c[idx];
+        }
+        return new ImmutableArrayList(newArray);
     }
 
     @Override
@@ -46,23 +74,29 @@ public final class ImmutableArrayList implements ImmutableList {
     }
 
     @Override
-    public int indexOf(Object e) {
-        return 0;
+    public int indexOf(Object e) throws NoSuchElementException {
+        for (int idx = 0; idx < length; idx++){
+            if (Objects.equals(e, array[idx])){
+                return idx;
+            }
+        }
+        throw new NoSuchElementException("No such element in array");
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.length;
     }
 
     @Override
     public ImmutableList clear() {
-        return null;
+        this.length = 0;
+        return new ImmutableArrayList();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.length == 0;
     }
 
     @Override
